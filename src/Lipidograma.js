cc9f4f8458cd6d273bd4add6578ec2110e567502
vitoria <vitoriajmota7@gmail.com>
2026-04-05 06:58:05 -0300

    0657

--- src/Lipidograma.js
+++ src/Lipidograma.js
@@ -1,6 +1,7 @@
 // COLE TUDO ISSO NO SEU ARQUIVO
 
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export default function Lipidograma() {
 
   const [form, setForm] = useState({
